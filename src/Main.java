63db4ee81eb63b69d5fbfc7df74f4da20f65a79e
YUSEONJUN <seonjun0906@gmail.com>
2023-01-23 01:38:16 +0900

    feat : README.md 추가

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+
+	public static void main(String[] args) {
+
+	}
+}
+
+class Test{
+	public long sum(int[] a) {
+		int sum = 0;
+		for (int i = 0; i <a.length; i++) {
+			sum += a[i];
+		}
+		return sum;
+	}
+}
