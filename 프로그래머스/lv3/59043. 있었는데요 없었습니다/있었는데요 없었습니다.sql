28fdf1d6402db910cf6712d810c7dd10d0b0e618
seonjun <110665247+YUSEONJUN@users.noreply.github.com>
2023-02-04 20:21:05 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT ins.ANIMAL_ID, ins.NAME
+from ANIMAL_INS ins
+join ANIMAL_OUTS outs 
+on ins.ANIMAL_ID = outs.ANIMAL_ID
+where ins.DATETIME > outs.DATETIME
+order by ins.DATETIME asc
