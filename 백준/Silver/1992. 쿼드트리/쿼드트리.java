101168bcc153a89cabd010401780d16e90d27184
seonjun <110665247+YUSEONJUN@users.noreply.github.com>
2023-02-14 13:55:38 +0900

    [Silver I] Title: 쿼드트리, Time: 128 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int N;
+	public static int[][] board;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+		board = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < N; j++) {
+				board[i][j] = str.charAt(j) - '0';
+			}
+		}
+
+		partition(0, 0, N);
+
+		System.out.println(sb);
+	}
+
+	public static void partition(int row, int col, int size) {
+		if (colorCheck(row, col, size)) {
+			sb.append(board[row][col]);
+			return;
+		}
+
+		int newSize = size / 2;
+
+		sb.append('(');
+
+		partition(row, col, newSize);
+		partition(row, col + newSize, newSize);
+		partition(row + newSize, col, newSize);
+		partition(row + newSize, col + newSize, newSize);
+		sb.append(')');
+
+	}
+
+	public static boolean colorCheck(int row, int col, int size) {
+
+		int color = board[row][col];
+		for (int i = row; i < row + size; i++) {
+			for (int j = col; j < col + size; j++) {
+				if (color != board[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+}
